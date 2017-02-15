d2077990a80ea46feff2aad948a6ab661df34f0d
Happy <happ.com>
2017-02-15 17:48:53 -0600

    created dinosaur added code

--- src/com/happy/Dinosaur.java
+++ src/com/happy/Dinosaur.java
@@ -4,4 +4,11 @@ package com.happy;
  * Created by Happy on 2/15/2017.
  */
 public class Dinosaur {
+    public static void main(String[] args) {
+
+      Dinosaur trex = new Dinosaur();
+      Dinosaur velociraptor = new Dinosaur();
+      trex.roar();
+      velociraptor.roar();
+    }
 }
---
+++ src/com/happy/Dinosaur2.java
@@ -0,0 +1,7 @@
+package com.happy;
+
+/**
+ * Created by Happy on 2/15/2017.
+ */
+public class Dinosaur2 {
+}
